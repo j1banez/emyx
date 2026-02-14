0b2f4811296a3db49ee4e3d64f95f26096257570
Jeremy Ibanez <jeremy.ibanez@protonmail.com>
2026-02-14 23:17:40 +0100

    Add printf %x and u32_to_hex

--- kernel/include/kernel/printk.h
+++ kernel/include/kernel/printk.h
@@ -1,6 +1,9 @@
 #ifndef _KERNEL_PRINTK_H
 #define _KERNEL_PRINTK_H
 
+#include <stdint.h>
+
 int printk(const char* format, ...);
+void u32_to_hex(char out[11], uint32_t value);
 
 #endif
--- kernel/lib/printk.c
+++ kernel/lib/printk.c
@@ -1,94 +1,137 @@
 #include <limits.h>
 #include <stdbool.h>
 #include <stdarg.h>
+#include <stdint.h>
 #include <string.h>
 
 #include <kernel/printk.h>
 #include <kernel/tty.h>
 
+static char hex_digit(uint8_t nibble)
+{
+    if (nibble < 10) {
+        return (char)('0' + nibble);
+    }
+
+    return (char)('a' + (nibble - 10));
+}
+
+void u32_to_hex(char out[11], uint32_t value)
+{
+    out[0] = '0';
+    out[1] = 'x';
+
+    for (int i = 0; i < 8; i++) {
+        uint8_t nibble = (uint8_t)((value >> ((7 - i) * 4)) & 0x0f);
+        out[2 + i] = hex_digit(nibble);
+    }
+
+    out[10] = '\0';
+}
+
 static bool print_bytes(const char* data, size_t length) {
-	terminal_write(data, length);
-	return true;
+    terminal_write(data, length);
+    return true;
 }
 
 int printk(const char* format, ...) {
-	va_list parameters;
-	va_start(parameters, format);
+    va_list parameters;
+    va_start(parameters, format);
 
-	int written = 0;
+    int written = 0;
 
-	while (*format != '\0') {
-		size_t maxrem = INT_MAX - written;
+    while (*format != '\0') {
+        size_t maxrem = INT_MAX - written;
 
-		if (format[0] != '%' || format[1] == '%') {
-			if (format[0] == '%') {
-				format++;
-			}
+        if (format[0] != '%' || format[1] == '%') {
+            if (format[0] == '%') {
+                format++;
+            }
 
-			size_t amount = 1;
-			while (format[amount] && format[amount] != '%') {
-				amount++;
-			}
+            size_t amount = 1;
+            while (format[amount] && format[amount] != '%') {
+                amount++;
+            }
 
-			if (maxrem < amount) {
-				return -1;
-			}
+            if (maxrem < amount) {
+                return -1;
+            }
 
-			if (!print_bytes(format, amount)) {
-				return -1;
-			}
+            if (!print_bytes(format, amount)) {
+                return -1;
+            }
 
-			format += amount;
-			written += amount;
-			continue;
-		}
+            format += amount;
+            written += amount;
+            continue;
+        }
 
-		const char* format_begun_at = format++;
+        const char* format_begun_at = format++;
 
-		if (*format == 'c') {
-			format++;
-			char c = (char) va_arg(parameters, int);
+        if (*format == 'c') {
+            format++;
+            char c = (char) va_arg(parameters, int);
 
-			if (!maxrem) {
-				return -1;
-			}
+            if (!maxrem) {
+                return -1;
+            }
 
-			if (!print_bytes(&c, sizeof(c))) {
-				return -1;
-			}
+            if (!print_bytes(&c, sizeof(c))) {
+                return -1;
+            }
 
-			written++;
-		} else if (*format == 's') {
-			format++;
-			const char* str = va_arg(parameters, const char*);
-			size_t len = strlen(str);
+            written++;
+        } else if (*format == 's') {
+            format++;
+            const char* str = va_arg(parameters, const char*);
 
-			if (maxrem < len) {
-				return -1;
-			}
+            if (!str) {
+                str = "(null)";
+            }
 
-			if (!print_bytes(str, len)) {
-				return -1;
-			}
+            size_t len = strlen(str);
 
-			written += len;
-		} else {
-			format = format_begun_at;
-			size_t len = strlen(format);
+            if (maxrem < len) {
+                return -1;
+            }
 
-			if (maxrem < len) {
-				return -1;
-			}
+            if (!print_bytes(str, len)) {
+                return -1;
+            }
 
-			if (!print_bytes(format, len)) {
-				return -1;
-			}
+            written += len;
+        } else if (*format == 'x') {
+            char hex[11];
 
-			written += len;
-			format += len;
-		}
-	}
+            format++;
+            u32_to_hex(hex, va_arg(parameters, uint32_t));
 
-	va_end(parameters);
-	return written;
+            if (maxrem < 10) {
+                return -1;
+            }
+
+            if (!print_bytes(hex, 10)) {
+                return -1;
+            }
+
+            written += 10;
+        } else {
+            format = format_begun_at;
+            size_t len = strlen(format);
+
+            if (maxrem < len) {
+                return -1;
+            }
+
+            if (!print_bytes(format, len)) {
+                return -1;
+            }
+
+            written += len;
+            format += len;
+        }
+    }
+
+    va_end(parameters);
+    return written;
 }
